314f9c1d788f5c1d4742fb6c7be576a047624cec
秋安 <1600858489@qq.com>
2023-11-09 17:19:34 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # backstageProject
+# ElementPlus
+* [官方文档](https://element-plus.org/zh-CN/):https://element-plus.org/zh-CN/
+# windi.css
+* [官方文档](https://cn.windicss.org/):https://cn.windicss.org/
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
+import 'virtual:windi.css'
+
 
 const app = createApp(App)
 
